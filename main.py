c0c0e38e782723b77a0c11b9c168b0ecf98f350c
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-12-19 16:11:30 +0530

    window ready

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app = Flask()
+app = Flask(__name__)
 
 @app.route("/")
 def index_page():
     return render_template("index.html")
+
+@app.route("/textacc")
+def accept_prompt():
+    prompt = request.args.get("message")
+    print(prompt)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
