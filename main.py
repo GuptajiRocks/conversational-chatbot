82781e19b1f87fd75940e33f7d5d4f48ee74c201
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-12-20 12:03:17 +0530

    textarea adding

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ app = Flask(__name__)
 def index_page():
     return render_template("index.html")
 
-@app.route("/textacc")
+@app.route("/textacc", methods=["POST"])
 def accept_prompt():
-    prompt = request.args.get("message")
-    print(prompt)
+    # prompt = request.args.get("message")
+    # print(prompt)
+
+    data = request.get_json()
+    prompt = data.get('message', '')
+    print(f"Received Message: {prompt}")
+
+    return render_template("test.html", mess=prompt)
 
 
 if __name__ == "__main__":
