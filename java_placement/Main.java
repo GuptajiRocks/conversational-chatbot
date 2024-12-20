a73fa73a9335cc0b543a2354391f852064b92a4e
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-12-20 16:52:26 +0530

    jesus-damn

---
+++ java_placement/Main.java
@@ -0,0 +1,10 @@
+import java_placement.Student;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.bulla();
+    }
+}
+
